f85d643438e6cf30db1612c2be707546110c46be
smugallimov <smugallimov@mail.ru>
2021-11-03 22:16:10 +0400

    NewsParser class is updated. The parsing is working correctly (main latest news from lenta.ru)

--- src/main/java/com/synergyproject/newsjavaapp/job/NewsParser.java
+++ src/main/java/com/synergyproject/newsjavaapp/job/NewsParser.java
@@ -2,6 +2,7 @@ package com.synergyproject.newsjavaapp.job;
 
 import com.synergyproject.newsjavaapp.model.News;
 import com.synergyproject.newsjavaapp.service.NewsService;
+import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,30 +21,27 @@ public class NewsParser {
         this.newsService = newsService;
     }
 
+    @SneakyThrows
     @Scheduled(fixedDelay = 10_000)
     public void parseNewNews(){
 
         String url = "https://lenta.ru/";
-        try {
-            Document document = Jsoup.connect(url)
-                    .userAgent("Google")
-                    .timeout(5_000)
-                    .referrer("https://www.google.com")
-                    .get();
+        Document document = Jsoup.connect(url)
+                .userAgent("Google")
+                .timeout(5_000)
+                .referrer("https://www.google.com")
+                .get();
 
-            Elements news = document.select("#root > section.b-layout.js-layout.b-layout_main > div > div > " +
+        Elements news = document.select("#root > section.b-layout.js-layout.b-layout_main > div > div > " +
                     "div.span4.b-sidebar-stickycolumn.js-main__sidebars > " +
                     "div.b-sidebar-stickycolumn__top.js-main__sidebar-top > section > div");
-            for(Element element: news.select("a")){
-                String title = element.text();
-                if(!newsService.isExist(title)){
-                    News obj = new News();
-                    obj.setTitle(title);
-                    newsService.save(obj);
-                }
+        for(Element element: news.select("a")){
+            String title = element.text();
+            if(!newsService.isExist(title)){
+                News obj = new News();
+                obj.setTitle(title);
+                newsService.save(obj);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
