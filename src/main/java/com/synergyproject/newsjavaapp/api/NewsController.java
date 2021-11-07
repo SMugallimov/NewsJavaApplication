d969ae6fe70d44de80fcc4b793efe084c6863394
smugallimov <smugallimov@mail.ru>
2021-11-07 22:25:56 +0400

    Added thymeleaf

--- src/main/java/com/synergyproject/newsjavaapp/api/NewsController.java
+++ src/main/java/com/synergyproject/newsjavaapp/api/NewsController.java
@@ -2,22 +2,31 @@ package com.synergyproject.newsjavaapp.api;
 
 import com.synergyproject.newsjavaapp.model.News;
 import com.synergyproject.newsjavaapp.service.NewsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
 public class NewsController {
 
     final NewsService newsService;
 
-    public NewsController(NewsService newsService) {
+    public NewsController (NewsService newsService){
         this.newsService = newsService;
     }
 
-    @GetMapping(value = "news")
+    //    @GetMapping(value = "/news")
     public List<News> getAllNews(){
         return newsService.getAllNews();
+    }
+
+    @GetMapping(value = "news")
+    public String newsMain(Model model){
+        model.addAttribute("news", newsService.getAllNews());
+        return "news-main";
     }
 }
